a36a0ef264ad2808b99e44e9091ec9f3f05b0fe5
RyanLaiber <ryanlaiber@gmail.com>
2021-11-03 11:11:45 -0300

    adiciona minima estilização, hoje estudei a respeito de useRef() para implementar a paginação de acordo com a visibilidade do ultimo elemento da lista de pokemons

--- src/components/PokeList/index.js
+++ src/components/PokeList/index.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPokePage } from '../../redux/actions';
+import './pokeList.css';
 
 
 function PokeList() {
   const [page, setPage] = useState(0);
   const dispatch = useDispatch();
   const list = useSelector(({ pokeList }) => pokeList);
-
+  const liRef = useRef();
+  const liVisible = null;
   useEffect(
     () => {
       dispatch(fetchPokePage(page))
@@ -15,10 +17,12 @@ function PokeList() {
   );
 
   return(
-    <div>
-      {(list.length > 0) && list.map( ({ name }) => <p key={ name }>{ name }</p>) }
+    <section>
+      <ul id="pokeList">
+        {(list.length > 0) && list.map( ({ name }) => <li key={ name }>{ name }</li>) }
+      </ul>
       <button onClick={ () => setPage(prev => prev + 1) }>mais 10</button>
-    </div>
+    </section>
   );
 }
 
