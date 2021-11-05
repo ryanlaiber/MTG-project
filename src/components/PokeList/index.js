a4de87162854bb5ee12862f97d14e27d074eaabb
RyanLaiber <ryanlaiber@gmail.com>
2021-11-05 09:40:42 -0300

    adiciona logica de paginaçao de acordo com visibilidade de elemento na lista.

--- src/components/PokeList/index.js
+++ src/components/PokeList/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPokePage } from '../../redux/actions';
 import './pokeList.css';
@@ -8,8 +8,20 @@ function PokeList() {
   const [page, setPage] = useState(0);
   const dispatch = useDispatch();
   const list = useSelector(({ pokeList }) => pokeList);
-  const liRef = useRef();
-  const liVisible = null;
+
+  const observer = useRef();
+  const lastPokemon = useCallback(node => {
+    if (observer.current) {
+      observer.current.disconnect();
+    }
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        setPage(prev => prev + 1);
+      }
+    });
+    if (node) observer.current.observe(node);
+  }, [setPage]);
+
   useEffect(
     () => {
       dispatch(fetchPokePage(page))
@@ -19,7 +31,12 @@ function PokeList() {
   return(
     <section>
       <ul id="pokeList">
-        {(list.length > 0) && list.map( ({ name }) => <li key={ name }>{ name }</li>) }
+        {(list.length > 0) && list.map( ({ name }, i) => {
+          if (i === (list.length -1)) {
+            return <li key={ i } ref={ lastPokemon }>{ name }</li>
+          }
+          return <li key={ i }>{ name }</li>
+      }) }
       </ul>
       <button onClick={ () => setPage(prev => prev + 1) }>mais 10</button>
     </section>
